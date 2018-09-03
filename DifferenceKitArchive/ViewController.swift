320c73b7d80b0f6081f2dde14c0525de606273e0
Stephan Diederich <stephan.diederich@gmail.com>
2018-09-03 09:47:57 +0200

    initial commit

---
+++ DifferenceKitArchive/ViewController.swift
@@ -0,0 +1,16 @@
+// Created by Stephan Diederich on 03.09.2018.
+// Copyright © 2018 Appossum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
